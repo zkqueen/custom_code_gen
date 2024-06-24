7419da249a420c6eccbfd78450261a2629c08781
zhoukan <zhoukan@flatincbr.com>
2024-06-24 21:09:40 +0800

    init

--- generators/lib/generators.dart
+++ generators/lib/generators.dart
@@ -1,14 +1,13 @@
 library generators;
 
 import 'package:build/build.dart';
-import 'package:generator/src/json_generator.dart';
+import 'package:generators/src/json_generator.dart';
 import 'package:source_gen/source_gen.dart';
-
 
 Builder generateJsonMethods(BuilderOptions options) {
   // Step 1
   return SharedPartBuilder(
     [JsonGenerator()], // Step 2
-    'json_generator', // Step 3
+    'kk', // Step 3
   );
 }
--- generators/lib/src/json_generator.dart
+++ generators/lib/src/json_generator.dart
@@ -1,7 +1,7 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:annotations/annotations.dart';
 import 'package:build/src/builder/build_step.dart';
-import 'package:generator/src/model_visitor.dart';
+import 'package:generators/src/model_visitor.dart';
 import 'package:source_gen/source_gen.dart';
 
 class JsonGenerator extends GeneratorForAnnotation<CustomAnnotation> {
